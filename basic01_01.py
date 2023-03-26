12f63a553eb8dca595689b7c6d0803563dd58f72
박소원 <126738099+sowonp@users.noreply.github.com>
2023-03-26 11:27:45 +0900

    Add files via upload
    basic01

---
+++ basic01_01.py
@@ -0,0 +1,11 @@
+print("정수를 입력하세요")
+
+a = int(input())
+b = int(input())
+
+print("a :", a, ',', "b :", b)
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
