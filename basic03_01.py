5d53398157fc855ea70f2a4215196abf70b58484
박소원 <126738099+sowonp@users.noreply.github.com>
2023-04-03 20:42:10 +0900

    Update basic03_01.py

--- basic03_01.py
+++ basic03_01.py
@@ -1,4 +1,6 @@
 true = True
+list_user = []
+
 while true == True:
     print("-----------")
     print("0. 종료")
@@ -6,7 +8,6 @@ while true == True:
     print("2. 로그인")
     print("-----------")
     a = input("입력: ")
-    list_user = []
 
     if a == "0":
         print("\n프로그램 종료")
@@ -16,6 +17,7 @@ while true == True:
         print("\n-----회원가입-----")
         list_user.insert(0, input("아이디 : "))
         list_user.insert(1, input("비밀번호 : "))
+        list_user.insert(2, input("이름 : "))
         print("\n--- 회원가입 성공!")
         continue
     
@@ -24,6 +26,6 @@ while true == True:
         id = input("아이디 : ")
         pw = input("비밀번호 : ")
         if id == list_user[0] and pw == list_user[1]:
-            print("\n--- {list_user[0]}님 환영합니다!")
+            print("\n---", list_user[2], "님 환영합니다!")
         else:
             print("아이디와 비밀번호를 확인해주세요.")
